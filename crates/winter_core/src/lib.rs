55e13c990fcee6984cf0beec04a11113680dedb4
Vinay Chandra Dommeti <vinaychandra.dommeti@gmail.com>
2021-10-20 08:29:56 -0700

    Minor cleanup
    - Remove `it_works` boilerplate test
    - Move leb128 to its own file in anticipation of more values support

--- crates/winter_core/src/lib.rs
+++ crates/winter_core/src/lib.rs
@@ -4,12 +4,3 @@
 extern crate no_std_compat as std;
 
 pub mod parse;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
