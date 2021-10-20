3f7a0df3d0c3a581de5c235184ee76bfec8985e5
Vinay Chandra Dommeti <vinaychandra.dommeti@gmail.com>
2021-10-20 10:17:41 -0700

    `name` value support
    Names are encoded as a vector of bytes containing the Unicode (Section
    3.9) UTF-8 encoding of the name’s character sequence.

--- crates/winter_core/src/parse/values/mod.rs
+++ crates/winter_core/src/parse/values/mod.rs
@@ -1,7 +1,9 @@
 mod leb128;
+mod name;
 mod vec;
 
 pub use leb128::*;
+pub use name::*;
 pub use vec::*;
 
 pub use nom::number::complete::f32 as float_f32;
---
+++ crates/winter_core/src/parse/values/name.rs
@@ -0,0 +1,35 @@
+use super::vector;
+use crate::parse::Res;
+use nom::error::{make_error, ContextError, ErrorKind, VerboseError};
+
+/// Names are encoded as a vector of bytes containing the Unicode
+/// UTF-8 encoding of the name’s character sequence.
+/// [Reference](https://webassembly.github.io/spec/core/binary/values.html#names)
+pub fn name(input: &[u8]) -> Res<&[u8], &str> {
+    vector(input).and_then(|(remaining, data)| {
+        core::str::from_utf8(data)
+            .map_err(|_| {
+                nom::Err::Error(VerboseError::add_context(
+                    input,
+                    "Unicode decoding failed",
+                    make_error(input, ErrorKind::Verify),
+                ))
+            })
+            .map(|inp| (remaining, inp))
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse::Res;
+
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        type ResType<'a> = Res<&'a [u8], &'a str>;
+
+        let value: ResType = name(&[0x2, 0x41, 0x42, 0x43]);
+        assert_eq!(value, Ok((&[0x43][..], "AB")));
+    }
+}
